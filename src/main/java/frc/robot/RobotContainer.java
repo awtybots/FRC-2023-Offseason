24b186b2cd99f09c558adfc7199765b64fbc551b
Mateo Cirino <24cirinom@awty.org>
2023-07-29 10:22:20 -0500

    test commit ignore

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,6 +1,8 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+//test shhh
+
 
 package frc.robot;
 
@@ -60,7 +62,6 @@ public class RobotContainer {
     private final WristSubsystem s_Wrist = new WristSubsystem();
     private final IntakeSubsystem s_Intake = new IntakeSubsystem();
 
-    private static boolean isCone = Constants.DefaultConfig.isCone;
     private static boolean resetPosMode = false;
     private static double angleOffset = 0;
 
@@ -171,13 +172,7 @@ public class RobotContainer {
         // Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared))));
     }
 
-    public static boolean getIsCone() {
-        return isCone;
-    }
 
-    public static void setIsCone(boolean value) {
-        isCone = value;
-    }
 
     public static boolean getResetPosMode() {
         return resetPosMode;
